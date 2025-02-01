4c20986b00c5c742622f9d0aaf2380b8e2bed417
bbair <56559910+bbair@users.noreply.github.com>
2025-01-31 20:24:03 -0700

    Initial conversion of color palette component

--- src/color_palette/color_palette.jsx
+++ src/color_palette/color_palette.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './color_palette.css'
 
 export function ColorPalette() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Color Palette displayed here</div>
+    <main className="text-center">
+        <div>
+            <h2 className="green-text">Current</h2>
+            <div className="palette-block">
+                <div className="color-block">
+                    <label for="current-color-main">Main (Grid): </label>
+                    <input type="color" id="current-color-main" name="current-color-main" disabled="true" value="#008000"/>
+                </div>
+                <div className="color-block">
+                    <label for="current-color-hit">Secondary (Hit): </label>
+                    <input type="color" id="current-color-hit" name="current-color-hit" disabled="true" value="#FF0000"/>
+                </div>
+            </div>
+            <h2 className="green-text">Customize Color Palette</h2>
+            <div className="palette-block">
+                <div className="color-block">
+                    <label for="current-color-main">Main (Grid): </label>
+                    <input type="color" id="custom-color-main" name="custom-color-main" value="#A020F0"/>
+                </div>
+                <div className="color-block">
+                    <label for="current-color-hit">Secondary (Hit): </label>
+                    <input type="color" id="custom-color-hit" name="custom-color-hit" value="#FFFF00"/>
+                </div>
+                <button type="submit" className="btn btn-secondary">Generate</button>
+            </div>
+        </div>
     </main>
   );
 }
