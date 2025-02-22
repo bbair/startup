c1ed1f3e5e85438e9d23302b1fbb98b865393716
bbair <56559910+bbair@users.noreply.github.com>
2025-02-22 11:37:25 -0700

    Putting the board into a separate component

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './play.css'
+import { Board } from './board';
+import './play.css';
 
 export function Play(props) {
   return (
@@ -15,25 +16,7 @@ export function Play(props) {
         <div>
           <h3 className="green-text">Your Board</h3>
           <svg width="320" height="320" xmlns="https://salvoattack.click/player-board">
-            <rect width="300" height="300" x="10" y="10" stroke="#606060" strokeWidth="3"/>
-            <line x1="10" y1="40" x2="310" y2="40" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="70" x2="310" y2="70" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="100" x2="310" y2="100" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="130" x2="310" y2="130" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="160" x2="310" y2="160" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="190" x2="310" y2="190" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="220" x2="310" y2="220" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="250" x2="310" y2="250" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="280" x2="310" y2="280" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="40" y1="10" x2="40" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="70" y1="10" x2="70" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="100" y1="10" x2="100" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="130" y1="10" x2="130" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="160" y1="10" x2="160" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="190" y1="10" x2="190" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="220" y1="10" x2="220" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="250" y1="10" x2="250" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="280" y1="10" x2="280" y2="310" stroke={props.gridColor} strokeWidth="3"/>
+            <Board gridColor={props.gridColor} />
             <ellipse rx="5" ry="10" cx="40" cy="40" fill={props.gridColor}/>
             <ellipse rx="5" ry="10" cx="250" cy="190" fill={props.gridColor}/>
             <ellipse rx="5" ry="10" cx="100" cy="130" fill={props.gridColor}/>
@@ -44,25 +27,7 @@ export function Play(props) {
         <div>
           <h3 className="green-text">Opponent's Board</h3>
           <svg width="320" height="320" xmlns="https://salvoattack.click/opponent-board">
-            <rect width="300" height="300" x="10" y="10" stroke="#606060" strokeWidth="3"/>
-            <line x1="10" y1="40" x2="310" y2="40" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="70" x2="310" y2="70" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="100" x2="310" y2="100" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="130" x2="310" y2="130" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="160" x2="310" y2="160" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="190" x2="310" y2="190" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="220" x2="310" y2="220" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="250" x2="310" y2="250" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="10" y1="280" x2="310" y2="280" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="40" y1="10" x2="40" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="70" y1="10" x2="70" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="100" y1="10" x2="100" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="130" y1="10" x2="130" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="160" y1="10" x2="160" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="190" y1="10" x2="190" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="220" y1="10" x2="220" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="250" y1="10" x2="250" y2="310" stroke={props.gridColor} strokeWidth="3"/>
-            <line x1="280" y1="10" x2="280" y2="310" stroke={props.gridColor} strokeWidth="3"/>
+            <Board gridColor={props.gridColor} />
             <ellipse rx="5" ry="10" cx="70" cy="40" fill={props.hitColor}/>
             <ellipse rx="5" ry="10" cx="100" cy="280" fill="#FFFFFF"/>
             <ellipse rx="5" ry="10" cx="280" cy="130" fill={props.hitColor}/>
