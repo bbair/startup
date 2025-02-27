b2939c18fd91e18cf5e29ed415732f9807205256
bbair <56559910+bbair@users.noreply.github.com>
2025-02-26 20:53:44 -0700

    Made legend a separate component

---
+++ src/play/legend.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export function Legend(props) {
+    return (
+        <>
+            <div className="text-center legend-box  bg-secondary">
+                <h6>Legend</h6>
+                <div className="legend-elements">
+                    <div className="element">
+                        <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
+                            <ellipse rx="5" ry="10" cx="5" cy="10" fill={props.gridColor}/>
+                        </svg>
+                    </div>
+                    <div className="element">
+                        <span>Ship</span>
+                    </div>
+                    <div className="element">
+                        <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
+                            <ellipse rx="5" ry="10" cx="5" cy="10" fill={props.hitColor}/>
+                        </svg>
+                    </div>
+                    <div className="element">
+                        <span>Hit</span>
+                    </div>
+                    <div className="element">
+                        <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
+                            <ellipse rx="5" ry="10" cx="5" cy="10" fill="#FFFFFF"/>
+                        </svg>
+                    </div>
+                    <div className="element">
+                        <span>Miss</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Board } from './board';
+import { Legend } from './legend';
 import './play.css';
 
 export function Play(props) {
@@ -23,53 +24,15 @@ export function Play(props) {
       <section className="text-center">
         <div>
           <h4 className="green-text">Your Board</h4>
-            <Board gridColor={props.gridColor} />
-            {/* <ellipse rx="5" ry="10" cx="40" cy="40" fill={props.gridColor}/>
-            <ellipse rx="5" ry="10" cx="250" cy="190" fill={props.gridColor}/>
-            <ellipse rx="5" ry="10" cx="100" cy="130" fill={props.gridColor}/>
-            <ellipse rx="5" ry="10" cx="190" cy="70" fill={props.gridColor}/>
-            <ellipse rx="5" ry="10" cx="70" cy="280" fill={props.hitColor}/> */}
+          <Board gridColor={props.gridColor} />
         </div>
         <div>
           <h4 className="green-text">Opponent's Board</h4>
-            <Board gridColor={props.gridColor} />
-            {/* <ellipse rx="5" ry="10" cx="70" cy="40" fill={props.hitColor}/>
-            <ellipse rx="5" ry="10" cx="100" cy="280" fill="#FFFFFF"/>
-            <ellipse rx="5" ry="10" cx="280" cy="130" fill={props.hitColor}/>
-            <ellipse rx="5" ry="10" cx="160" cy="100" fill="#FFFFFF"/>
-            <ellipse rx="5" ry="10" cx="250" cy="250" fill="#FFFFFF"/> */}
+          <Board gridColor={props.gridColor} />
         </div>
       </section>
 
-      <div className="text-center legend-box  bg-secondary">
-        <h6>Legend</h6>
-        <div className="legend-elements">
-          <div className="element">
-            <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
-              <ellipse rx="5" ry="10" cx="5" cy="10" fill={props.gridColor}/>
-            </svg>
-          </div>
-          <div className="element">
-            <span>Ship</span>
-          </div>
-          <div className="element">
-            <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
-              <ellipse rx="5" ry="10" cx="5" cy="10" fill={props.hitColor}/>
-            </svg>
-          </div>
-          <div className="element">
-            <span>Hit</span>
-          </div>
-          <div className="element">
-            <svg width="10" height="20" xmlns="https://salvoattack.click/ship">
-              <ellipse rx="5" ry="10" cx="5" cy="10" fill="#FFFFFF"/>
-            </svg>
-          </div>
-          <div className="element">
-            <span>Miss</span>
-          </div>
-        </div>
-      </div>
+      <Legend gridColor={props.gridColor} hitColor={props.hitColor} />
     </main>
   );
 }
