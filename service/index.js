5ee08e9a083193143c8ebf716ab57e4bc0a606dd
bbair <56559910+bbair@users.noreply.github.com>
2025-03-01 22:17:24 -0700

    Adding some things to the app

--- service/index.js
+++ service/index.js
@@ -4,14 +4,21 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
 
-app.use(express.json());
-
 // The users, colors, and positions are saved in memory and disappear whenever the service is restarted.
 let users = [];
 let colors = [];
 let positions = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
